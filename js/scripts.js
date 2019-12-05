9d59c0249e1fe04b55ea940b110306d9852344dd
Nicholas Roberto <njr444@gmail.com>
2019-12-05 12:32:01 -0800

    finished quesions and formatting in html/css. added variables and click event handlers on the input classes to capture values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
 var selectedLogo;
 var selectedGenre;
 var selectedHoliday;
-
+var selectedDrink;
+var selectedMovie;
+var selectedFlavor;
 
 
 $('.logo-select').click(function() {
     console.log(this.value);
     selectedLogo = this.value;
+  });
+  $('.genre-select').click(function() {
+    console.log(this.value);
+    selectedGenre = this.value;
+  });
+  $('.holiday-select').click(function() {
+    console.log(this.value);
+    selectedHoliday = this.value;
+  });
+  $('.movie-select').click(function() {
+    console.log(this.value);
+    selectedMovie = this.value;
+  });
+  $('.flavor-select').click(function() {
+    console.log(this.value);
+    selectedFlavor = this.value;
+  });
+  $('.drink-select').click(function() {
+    console.log(this.value);
+    selectedDrink = this.value;
   });
