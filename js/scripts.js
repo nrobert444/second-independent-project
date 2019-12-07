fa511250902af104b444dadb8ff1851777c1fd55
Nicholas Roberto <njr444@gmail.com>
2019-12-07 09:27:06 -0800

    removed console.log commands, added document.ready to jquery, updated css button styling.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+$(document).ready( function (){
+});
+
 var selectedLogo;
 var selectedGenre;
 var selectedHoliday;
@@ -7,27 +10,21 @@ var selectedFlavor;
 
 
 $('.logo-select').click(function() {
-    console.log(this.value);
     selectedLogo = this.value;
   });
   $('.genre-select').click(function() {
-    console.log(this.value);
     selectedGenre = this.value;
   });
   $('.holiday-select').click(function() {
-    console.log(this.value);
     selectedHoliday = this.value;
   });
   $('.movie-select').click(function() {
-    console.log(this.value);
     selectedMovie = this.value;
   });
   $('.flavor-select').click(function() {
-    console.log(this.value);
     selectedFlavor = this.value;
   });
   $('.drink-select').click(function() {
-    console.log(this.value);
     selectedDrink = this.value;
   });
 
