0570f383be8d4c79c034bea3e142adb6ded2db38
Nicholas Roberto <njr444@gmail.com>
2019-12-07 09:05:08 -0800

    add conditional statements to scripts file to collect points based on asnwers selected on survey.

--- js/scripts.js
+++ js/scripts.js
@@ -30,3 +30,57 @@ $('.logo-select').click(function() {
     console.log(this.value);
     selectedDrink = this.value;
   });
+
+  if (selectedLogo === 'snake') {
+    python += 1;
+  } else if (selectedLogo === 'letters') {
+    javascript += 1;
+  } else if (selectedLogo === 'gem') {
+    ruby += 1;
+  } else {
+  }
+  
+  if (selectedGenre === 'horror') {
+    python += 1;
+  } else if (selectedGenre === 'comedy') {
+    javascript += 1;
+  } else if (selectedGenre === 'drama') {
+    ruby += 1;
+  } else {
+  }
+  
+  if (selectedHoliday === 'halloween') {
+    python += 1;
+  } else if (selectedHoliday === 'thanksgiving') {
+    javascript += 1;
+  } else if (selectedHoliday === 'christmas') {
+    ruby += 1;
+  } else {
+  }
+  
+  if (selectedFlavor === 'vanilla') {
+    python += 1;
+  } else if (selectedFlavor === 'strawberry') {
+    javascript += 1;
+  } else if (selectedFlavor === 'chocolate') {
+    ruby += 1;
+  } else {
+  }
+  
+  if (selectedMovie === 'jaws') {
+    python += 1;
+  } else if (selectedMovie === 'jurassicpark') {
+    javascript += 1;
+  } else if (selectedMovie === 'shindlersList') {
+    ruby += 1;
+  } else {
+  }
+  
+  if (selectedDrink === 'beer') {
+    python += 1;
+  } else if (selectedDrink === 'wine') {
+    javascript += 1;
+  } else if (selectedDrink === 'water') {
+    ruby += 1;
+  } else {
+  }
